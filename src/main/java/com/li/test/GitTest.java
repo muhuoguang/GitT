705d3e98b7e24eaf8151ca12a7282e2fb1b0253d
liyihui <88970180+muhuoguang@users.noreply.github.com>
2021-08-16 00:09:17 +0800

    Update GitTest.java

--- src/main/java/com/li/test/GitTest.java
+++ src/main/java/com/li/test/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("second");
         System.out.println("master");
         System.out.println("hot");
+        System.out.println("change");
     }
 }
